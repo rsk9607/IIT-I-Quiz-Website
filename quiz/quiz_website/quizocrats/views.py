c00bf93e7bce12243ed4d4b3c340ec8284b1ea86
Sriyans-Reddy <vsriyansreddy2005@gmail.com>
2023-06-30 22:00:46 +0530

    after login

--- quiz/quiz_website/quizocrats/views.py
+++ quiz/quiz_website/quizocrats/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.models import User
-from django.contrib.auth import authenticate
+from django.contrib.auth import authenticate,login
+from django.contrib import messages
 
 # Create your views here.
 def home(request):
@@ -11,20 +12,61 @@ def home(request):
     pas=request.POST.get('password')
     user=authenticate(username=name, password=pas)
     if user is not None:
-      return HttpResponse('done')
-  return render(request, 'home.html')
+      
+      return render(request,'home2.html')
+    else:
+      messages.warning(request, 'Wrong Username or password')
+      return render(request, 'home.html')
+
+  else:  
+    return render(request, 'home.html')
+
 def contact(request):
-  template = loader.get_template('contact.html')
-  return HttpResponse(template.render())
+  if request.method=='POST':
+    name=request.POST.get('username')
+    pas=request.POST.get('password')
+    user=authenticate(username=name, password=pas)
+    if user is not None:
+      return HttpResponse('done')
+    else:
+      messages.warning(request, 'Wrong Username or password')
+      return render(request, 'contact.html')
+
+  else:  
+    return render(request, 'contact.html')
+
 def aboutus(request):
-  template = loader.get_template('aboutus.html')
-  return HttpResponse(template.render())
+  if request.method=='POST':
+    name=request.POST.get('username')
+    pas=request.POST.get('password')
+    user=authenticate(username=name, password=pas)
+    if user is not None:
+      return HttpResponse('done')
+    else:
+      messages.warning(request, 'Wrong Username or password')
+      return render(request, 'aboutus.html')
+
+  else:  
+    return render(request, 'aboutus.html')
+
 def help(request):
-  template = loader.get_template('help.html')
-  return HttpResponse(template.render())
+  if request.method=='POST':
+    name=request.POST.get('username')
+    pas=request.POST.get('password')
+    user=authenticate(username=name, password=pas)
+    if user is not None:
+      return HttpResponse('done')
+    else:
+      messages.warning(request, 'Wrong Username or password')
+      return render(request, 'help.html')
+
+  else:  
+    return render(request, 'help.html')
+
 def samquiz(request):
   template = loader.get_template('quizpage.html')
   return HttpResponse(template.render())
+
 def quizanswers(request):
   template = loader.get_template('quizanswers.html')
   return HttpResponse(template.render())
