ca6d5c65a3d1bb1ff22ea0e07e6da7d0f7744317
Sriyans-Reddy <vsriyansreddy2005@gmail.com>
2023-07-03 12:18:45 +0530

    creating user by signup

--- quiz/quiz_website/quizocrats/views.py
+++ quiz/quiz_website/quizocrats/views.py
@@ -2,22 +2,42 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import loader
 from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from django.contrib.auth.hashers import make_password,check_password
 from django.contrib.auth import authenticate,login,logout
 from django.contrib import messages
 from django.shortcuts import redirect
 # Create your views here.
 def home(request):
   if request.method=='POST':
-    name=request.POST.get('username')
-    pas=request.POST.get('password')
-    user=authenticate(username=name, password=pas)
-    if user is not None:
+    if 'pass1' in request.POST:
+      name=request.POST.get('username')
+      pas1=request.POST.get('pass1')
+      pas2=request.POST.get('pass2')
+      email=request.POST.get('emailid')
+      if len(name)>20:
+        messages.warning(request, 'use shorter name')
+        return render(request, 'home.html')
+      
+      if pas1!=pas2:
+        messages.warning(request, 'password not same')
+        return render(request, 'home.html')
+      
+      user=User.objects.create(username=name,email=email,password=make_password(pas1))
+      user.save()
+      user=authenticate(username=name, password=pas1)
       login(request,user)
       return redirect('home')
     else:
-      messages.warning(request, 'Wrong Username or password')
-      return render(request, 'home.html')
-
+      name=request.POST.get('username')
+      pas=request.POST.get('password')
+      user=authenticate(username=name, password=pas)
+      if user is not None:
+        login(request,user)
+        return redirect('home')
+      else:
+        messages.warning(request, 'Wrong Username or password')
+        return render(request, 'home.html')
   else: 
     if request.user.is_authenticated:
       return render(request,'home2.html') 
@@ -25,45 +45,102 @@ def home(request):
 
 def contact(request):
   if request.method=='POST':
-    name=request.POST.get('username')
-    pas=request.POST.get('password')
-    user=authenticate(username=name, password=pas)
-    if user is not None:
+    if 'pass1' in request.POST:
+      name=request.POST.get('username')
+      pas1=request.POST.get('pass1')
+      pas2=request.POST.get('pass2')
+      email=request.POST.get('emailid')
+      if len(name)>20:
+        messages.warning(request, 'use shorter name')
+        return render(request, 'home.html')
+      
+      if pas1!=pas2:
+        messages.warning(request, 'password not same')
+        return render(request, 'home.html')
+      
+      user=User.objects.create(username=name,email=email,password=make_password(pas1))
+      user.save()
+      user=authenticate(username=name, password=pas1)
       login(request,user)
-      return render(request,'home2.html')
+      return redirect('home')
     else:
-      messages.warning(request, 'Wrong Username or password')
-      return render(request, 'contact.html')
+      name=request.POST.get('username')
+      pas=request.POST.get('password')
+      user=authenticate(username=name, password=pas)
+      if user is not None:
+        login(request,user)
+        return redirect('home')
+      else:
+        messages.warning(request, 'Wrong Username or password')
+        return render(request, 'contact.html')
 
   else:  
     return render(request, 'contact.html')
 
 def aboutus(request):
   if request.method=='POST':
-    name=request.POST.get('username')
-    pas=request.POST.get('password')
-    user=authenticate(username=name, password=pas)
-    if user is not None:
+    if 'pass1' in request.POST:
+      name=request.POST.get('username')
+      pas1=request.POST.get('pass1')
+      pas2=request.POST.get('pass2')
+      email=request.POST.get('emailid')
+      if len(name)>20:
+        messages.warning(request, 'use shorter name')
+        return render(request, 'home.html')
+      
+      if pas1!=pas2:
+        messages.warning(request, 'password not same')
+        return render(request, 'home.html')
+      
+      user=User.objects.create(username=name,email=email,password=make_password(pas1))
+      user.save()
+      user=authenticate(username=name, password=pas1)
       login(request,user)
-      return render(request,'home2.html')
+      return redirect('home')
     else:
-      messages.warning(request, 'Wrong Username or password')
-      return render(request, 'aboutus.html')
+      name=request.POST.get('username')
+      pas=request.POST.get('password')
+      user=authenticate(username=name, password=pas)
+      if user is not None:
+        login(request,user)
+        return redirect('home')
+      else:
+        messages.warning(request, 'Wrong Username or password')
+        return render(request, 'aboutus.html')
 
   else:  
     return render(request, 'aboutus.html')
 
 def help(request):
   if request.method=='POST':
-    name=request.POST.get('username')
-    pas=request.POST.get('password')
-    user=authenticate(username=name, password=pas)
-    if user is not None:
+    if 'pass1' in request.POST:
+      name=request.POST.get('username')
+      pas1=request.POST.get('pass1')
+      pas2=request.POST.get('pass2')
+      email=request.POST.get('emailid')
+      if len(name)>20:
+        messages.warning(request, 'use shorter name')
+        return render(request, 'home.html')
+      
+      if pas1!=pas2:
+        messages.warning(request, 'password not same')
+        return render(request, 'home.html')
+      
+      user=User.objects.create(username=name,email=email,password=make_password(pas1))
+      user.save()
+      user=authenticate(username=name, password=pas1)
       login(request,user)
-      return render(request,'home2.html')
+      return redirect('home')
     else:
-      messages.warning(request, 'Wrong Username or password')
-      return render(request, 'help.html')
+      name=request.POST.get('username')
+      pas=request.POST.get('password')
+      user=authenticate(username=name, password=pas)
+      if user is not None:
+        login(request,user)
+        return redirect('home')
+      else:
+        messages.warning(request, 'Wrong Username or password')
+        return render(request, 'help.html')
 
   else:  
     return render(request, 'help.html')
