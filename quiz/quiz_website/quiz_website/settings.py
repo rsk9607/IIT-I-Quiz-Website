16176f7b56620e604e20b2a24e7174ca1e7742d3
Sriyans-Reddy <vsriyansreddy2005@gmail.com>
2023-07-25 17:55:03 +0530

    statics

--- quiz/quiz_website/quiz_website/settings.py
+++ quiz/quiz_website/quiz_website/settings.py
@@ -115,10 +115,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
-
+STATIC_ROOT = BASE_DIR / 'public'
 STATIC_URL = 'static/'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField' 
